2f8f742754eaf5bd3b65664d8a5abb812747dfdf
Erfan Rasti <erfanrasty@gmail.com>
2022-06-04 17:17:09 +0000

    Implementing gradient descent algorithm
    The sketch of gradient_descent function
    has been implemented in the "q1.h" file.

--- include/q1.h
+++ include/q1.h
@@ -1,4 +1,39 @@
 #ifndef Q1_H
 #define Q1_H
 
-#endif //Q1_H
+namespace q1
+{
+    template <typename T, typename Func>
+    double gradient_descent(T initialValue, T stepSize, Func f = Func{})
+    {
+        /**
+         * @brief gradient descent algorithm
+         *
+         * @param initialValue initial value of the variable
+         * @param stepSize step size of the algorithm
+         * @param f function to minimize
+         * @return T minimum value of the function
+         */
+
+        // Defining the variables
+        T x{initialValue};
+        T fx{f(x)};
+
+        while (true)
+        {
+            // Checking the orientation of the gradient
+            if (fx > f(x + stepSize))
+                x += stepSize;
+            else if (fx > f(x - stepSize))
+                x -= stepSize;
+            else
+                break;
+
+            fx = f(x);
+        }
+
+        return x;
+    }
+}
+
+#endif // Q1_H
