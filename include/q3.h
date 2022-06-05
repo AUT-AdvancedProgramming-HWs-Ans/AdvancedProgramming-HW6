20d12c409103686a4997e012bbb5f0adb62f9e6c
Erfan Rasti <erfanrasty@gmail.com>
2022-06-05 14:24:40 +0000

    Implementing the "timeToMinuteConverter" function
    The "timeToMinuteConverter" function has been implemented
    in the "q3.h" file.

--- include/q3.h
+++ include/q3.h
@@ -54,6 +54,30 @@ namespace q4
             return flight1.duration + flight1.connection_times + 3 * flight1.price >
                    flight2.duration + flight2.connection_times + 3 * flight2.price;
         }};
+
+    inline size_t timeToMinuteConverter(std::string time)
+    {
+        /**
+         * @brief convert time to minutes
+         *
+         * @param time time in format hh:mm
+         *
+         * @return size_t
+         */
+
+        if (time.empty())
+            return 0;
+
+        std::smatch match{};
+        std::regex pattern(R"((\d+)\h(\d+)?\m?)");
+        std::regex_search(time, match, pattern);
+        
+        return static_cast<size_t>(std::stoi(match[1])) * 60 +
+               (((static_cast<std::string>(match[2])).empty())
+                    ? 0
+                    : static_cast<size_t>(std::stoi(match[2])));
+    }
+
 } // namespace q4
 
 #endif // Q3_H
